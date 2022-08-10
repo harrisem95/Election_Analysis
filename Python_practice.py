fd62e27e64cf743b1172dde618ff544edca00395
Emily Harris <emily@Chriss-Air.lan>
2022-08-10 07:10:49 -0400

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,9 @@
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
+
+for dict in voting_data:
+    print(f" {dict['county']} has {dict['registered_voters']} registered voters.")
+        
+
+
+
+
