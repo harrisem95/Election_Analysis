c2fbfc326bf4b7657a26e3d7370997863c2cf1be
harrisem95 <107651183+harrisem95@users.noreply.github.com>
2022-08-14 14:10:55 -0400

    Delete Python_practice.py

--- Python_practice.py
+++
@@ -1,9 +0,0 @@
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
-
-for dict in voting_data:
-    print(f" {dict['county']} has {dict['registered_voters']} registered voters.")
-        
-
-
-
-
